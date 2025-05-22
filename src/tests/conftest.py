b8a9ea835f58924b8f15922e24d4b96aeedc80f6
Mark Dickerson <mark.forrest.dickerson+GitHub@gmail.com>
2025-05-22 17:19:54 -0400

    Unit tests\!

---
+++ src/tests/conftest.py
@@ -0,0 +1,11 @@
+"""
+Pytest fixtures and settings for the unit tests.
+"""
+
+import aiohttp
+import pytest_asyncio
+
+@pytest_asyncio.fixture()
+async def client_session():
+    async with aiohttp.ClientSession(trust_env=True) as client_session:
+        return client_session
